de40027a3ae98a82931f7d6533e66dee6eb76cfa
Igor <igor.zamaraev@gcorelabs.com>
2021-09-16 15:38:06 +0500

    docker build & run.

---
+++ sh/docker_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+project_src="$HOME/project_src"
+project_dir="$HOME/web_project"
+
+mkdir "$project_dir"
+sudo docker build -t web_im "$project_src/BE"
+sudo docker run -it --rm --name web_cont -p 5000:5000 web_im
--- sh/git_clone.sh
+++ sh/git_clone.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 repo_url="http://github.com/Beellzebub/multi_tierApp"
+project_src="$HOME/project_src"
 
 function git_clone {
-    git clone "$1"
+    git clone "$1" "$2"
 }
 
-git_clone $repo_url
+mkdir "$project_src"
+git_clone $repo_url "$project_src"
