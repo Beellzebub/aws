5011600e9a91c7fe2b1ad33f523140c5bd24b021
Igor <igor.zamaraev@gcorelabs.com>
2021-09-17 17:47:21 +0500

    change path for app.

--- sh/docker_run.sh
+++ sh/docker_run.sh
@@ -4,5 +4,5 @@ project_src="$HOME/project_src"
 project_dir="$HOME/web_project"
 
 mkdir "$project_dir"
-sudo docker build -t web_im "$project_src/BE"
+sudo docker build -t web_im "$project_src/application"
 sudo docker run -it --rm --name web_cont -p 5000:5000 web_im
