da5868f2a0d358d4248adb73b1f2a0a3a61d3292
Igor <eniumig@gmail.com>
2021-10-15 17:12:54 +0500

    Install aws cli.

--- sh/aws_cli/aws_cli_install.sh
+++ sh/aws_cli/aws_cli_install.sh
@@ -1,1 +1,9 @@
-cat /etc/os-release | grep "ID=ubuntu" "ID=amzn"
+#!/bin/bash
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+sudo ./aws/install
+
+rm -r aws
+rm awscliv2.zip
