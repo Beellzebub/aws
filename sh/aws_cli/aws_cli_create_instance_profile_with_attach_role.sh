5f58e7a3eda0e4125fdd2d769e56acd35bb85694
Igor <eniumig@gmail.com>
2021-10-17 03:17:29 +0500

    two options of create instance profile.

---
+++ sh/aws_cli/aws_cli_create_instance_profile_with_attach_role.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+aws iam create-role \
+--role-name  EC2toS3FullAccess \
+--description "Allow EC2 to do everything with any S3 buckets" \
+--assume-role-policy-document file://./json/ec2-role-trust-policy.json
+
+aws iam attach-role-policy \
+--role-name EC2toS3FullAccess \
+--policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+
+aws iam create-instance-profile \
+--instance-profile-name EC2toS3FullAccessProfile
+
+aws iam add-role-to-instance-profile \
+--instance-profile-name EC2toS3FullAccessProfile \
+--role-name  EC2toS3FullAccess
