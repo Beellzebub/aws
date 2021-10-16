244362c63fa7f9b6ebc46d1e3c9cf4f710d7cc91
Igor <eniumig@gmail.com>
2021-10-16 21:48:19 +0500

    add instance name in script.

--- sh/aws_cli/aws_cli_terminate_instance.sh
+++ sh/aws_cli/aws_cli_terminate_instance.sh
@@ -2,8 +2,10 @@
 
 # Terminate instance:
 
+instance_name="ubuntu-ud1"
+
 instance_id=$(aws ec2 describe-instances \
---filters "Name=tag:Name,Values=ubuntu-cli" \
+--filters "Name=tag:Name,Values=$instance_name" \
 --query "Reservations[].Instances[].InstanceId" \
 --output text)
 
