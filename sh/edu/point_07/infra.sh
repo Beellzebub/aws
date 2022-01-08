76267ebf8c8e920f1a527ea03789f9bc25c24808
Igor <eniumig@gmail.com>
2022-01-09 02:27:43 +0500

    checkpoint 07 (v.04)

--- sh/edu/point_07/infra.sh
+++ sh/edu/point_07/infra.sh
@@ -4,7 +4,7 @@
 function vpc {
   status_of_default_VPC="$(aws ec2 describe-vpcs \
   --filters "Name=isDefault, Values=true" \
-  --query "Vpcs[*].IsDefault" \
+  --query "Vpcs[].IsDefault" \
   --output text \
   --region "$1")"
 
@@ -45,8 +45,53 @@ function subnet {
       aws ec2 create-default-subnet --availability-zone "$zone" --region "$1"
       echo "Default subnet in $zone zone created."
     fi
-
   done
+}
+
+# $1-region $2-group name $3-list of ports
+function security {
+	select_group="$(aws ec2 describe-security-groups \
+	--filters "Name=group-name, Values=$2" \
+	--query "SecurityGroups[].GroupName" \
+	--region "$1" \
+	--output text)"
+
+	if [[ -z "$select_group" ]]; then
+
+	  group_id="$(aws ec2 create-security-group \
+	  --description "$2" \
+	  --group-name "$2" \
+	  --query "GroupId" \
+	  --region "$1" \
+	  --output text)"
+	  echo "Group $2 created."
+
+    for port in $3; do
+      aws ec2 authorize-security-group-ingress \
+      --group-id "$group_id" \
+      --protocol "tcp" \
+      --port "$port" \
+      --cidr 0.0.0.0/0 \
+      --region "$1"
+    done
+	else
+		echo "Group $2 exists."
+  fi
+}
+
+# $1-region $2-key name $3-key type
+function key {
+  select_key="$(aws ec2 describe-key-pairs \
+	--filters "Name=key-name, Values=$2" \
+	--query "KeyPairs[].KeyName" \
+	--region "$1" \
+	--output text)"
+
+	if [[ -z "$select_key" ]]; then
+	  echo "Key $2 created."
+	else
+		echo "Key $2 exists."
+  fi
 }
 
 arr="$(cat regions-allowed.conf)"
@@ -56,5 +101,8 @@ for region in $arr; do
   echo "Region: $region"
   vpc "$region"
   subnet "$region"
+  security "$region" public-ssh-and-http "22 80"
+  security "$region" public-ssh-http-81 "22 80 81"
+  key "$region" student-ed25519 ed25519
+  key "$region" student-rsa rsa
 done
-
